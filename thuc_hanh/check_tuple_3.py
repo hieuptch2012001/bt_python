021332c858c6d3376951e3b8c23a4af3f1b92006
Pham_Hieu <hieuptch2012001@fpt.edu.vn>
2021-05-15 21:55:42 +0700

    update check_tuple_3.py

--- thuc_hanh/check_tuple_3.py
+++ thuc_hanh/check_tuple_3.py
@@ -1,17 +1,1 @@
-
-# Python3 code to demonstrate
-# test for values in tuple of tuple
-# using any()
-
-# initializing tuple of tuple
-test_tuple = (("geeksforgeeks", "gfg"), ("CS_Portal", "best"))
-
-# printing tuple
-print("The original tuple is " + str(test_tuple))
-
-# using any()
-# to test for value in tuple of tuple
-if (any('geeksforgeeks' in i for i in test_tuple)):
-    print("geeksforgeeks is present")
-else:
-    print("geeksforgeeks is not present")
+# chưa hiểu đầu bài
